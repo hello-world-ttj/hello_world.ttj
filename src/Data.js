cb3262bd0e43edf469ffa4c613fbeca79ceefb0c
Tijo T Joseph <tijotjoseph@Tijos-MacBook-Air.local>
2023-01-03 20:39:23 +0530

    Data for project...

---
+++ src/Data.js
@@ -0,0 +1,84 @@
+let Data = [
+    {
+        id: "001",
+        title: "M-D",
+        img: "images/M-D.jpg",
+        lang: "React",
+        des: "Movie detailing app",
+        link: "https://m-d.netlify.app/"
+    },
+    {
+        id: "002",
+        title: "Day Time Date",
+        img: "images/Daytimedate.jpg",
+        lang: "React",
+        des: "Just like clock and calendar",
+        link: "https://daytimedate.netlify.app/"
+    },
+    {
+        id: "003",
+        title: "Memes Generator",
+        img: "images/Meme.jpeg",
+        lang: "React",
+        des: "We can create meme through this web app",
+        link: "https://hello-world-ttj-meme-generator.netlify.app/"
+    },
+    {
+        id: "004",
+        title: "ToDo",
+        img: "images/Todo.jpeg",
+        lang: "React",
+        des: "Its a todo app",
+        link: "https://hello-world-ttj-todo-app.netlify.app/"
+    },
+    {
+        id: "005",
+        title: "Black Jack",
+        img: "images/black.jpeg",
+        lang: "JS",
+        des: "Black Jack Game",
+        link: "https://blackjackgame21.netlify.app/"
+    },
+    {
+        id: "006",
+        title: "Rock Paper Scissors",
+        img: "images/Rps.jpeg",
+        lang: "JS",
+        des: "Rock Paper Scissors Game",
+        link: "https://hello-world-ttj-rps.netlify.app/"
+    },
+    {
+        id: "007",
+        title: "Kurup",
+        img: "images/kurup.jpeg",
+        lang: "React",
+        des: "Kurup movie details",
+        link: "https://kurup.netlify.app/"
+    },
+    {
+        id: "008",
+        title: "Counter",
+        img: "images/counter.jpeg",
+        lang: "JS",
+        des: "Counter App",
+        link: "https://numbercounting.netlify.app/"
+    },
+    {
+        id: "009",
+        title: "Apple Clone",
+        img: "images/apple.png",
+        lang: "Node JS, Handlebars & MongoDB",
+        des: "BCA Main Project",
+        link: ""
+    },
+    {
+        id: "010",
+        title: "Educa",
+        img: "images/educa.jpeg",
+        lang: "PHP & MYSQL",
+        des: "BCA Mini Project",
+        link: ""
+    }
+]
+
+export default Data
