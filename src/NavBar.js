ce41bafa1068c0e6d53cc7e7dc40080ef86b547d
Tijo T Joseph <tijotjoseph@Tijos-MacBook-Air.local>
2023-01-03 19:36:47 +0530

    Changes added...

--- src/NavBar.js
+++ src/NavBar.js
@@ -9,7 +9,7 @@ function NavBar() {
     <img
       src={Logo}
       className="mr-3 h-6 sm:h-9 md:mt-2"
-      alt="Flowbite Logo"
+      alt="Logo"
     />
     <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white md:pt-2">
         TIJO T JOSEPH <span className='text-base'>/ Developer</span>
@@ -18,7 +18,8 @@ function NavBar() {
   <Navbar.Toggle />
   <Navbar.Collapse className='pt-4'>
         <NavLink className="block py-2 pr-4 pl-3 md:p-0 md:pt-2 border-b border-gray-100  text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white" to="/">About Me</NavLink>
-        <NavLink className="block py-2 pr-4 pl-3 md:p-0 md:pt-2 border-b border-gray-100  text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white" to="/resume">Resume</NavLink>
+        <NavLink className="block py-2 pr-4 pl-3 md:p-0 md:pt-2 border-b border-gray-100  text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white" to="/resume">Skills</NavLink>
+        <NavLink className="block py-2 pr-4 pl-3 md:p-0 md:pt-2 border-b border-gray-100  text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white" to="/education">Education</NavLink>
         <NavLink className="block py-2 pr-4 pl-3 md:p-0 md:pt-2 border-b border-gray-100  text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white" to="/projects">Projects</NavLink>
         <NavLink className="block py-2 pr-4 pl-3 md:p-0 md:pt-2 border-b border-gray-100  text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white" to="/contact">Contact</NavLink>
     <Flowbite >
